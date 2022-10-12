6be3925002526e3eed214b642bc78e5db0d1de11
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-10-12 20:41:32 +0300

    add files

---
+++ src/com/company/IntsCalculator.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import static com.company.Calculator.Operation.*;
+
+public class IntsCalculator implements Ints {
+
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(SUM)
+                .result();
+
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(MULT)
+                .result();
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(POW)
+                .result();
+    }
+
+
+}
